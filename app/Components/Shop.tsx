37978d047f09cf70f7faa725614224bbf155a0e4
Dabhi vaishali balvantbhai <dabhivaishali127@gmail.com>
2025-12-29 13:23:32 +0530

    change

--- app/Components/Shop.tsx
+++ app/Components/Shop.tsx
@@ -4,37 +4,37 @@ const products = [
   {
     id: 1,
     name: "Denim Jacket",
-    img: "https://images.unsplash.com/photo-1541099649105-f69ad21f3246",
+    img: "/Img/pic7.webp",
     price: "$45",
   },
   {
     id: 2,
     name: "Gray T-Shirt",
-    img: "https://images.unsplash.com/photo-1521572163474-6864f9cf17ab",
+    img: "/Img/pic6.webp",
     price: "$30",
   },
   {
     id: 3,
     name: "White Top",
-    img: "https://images.unsplash.com/photo-1520975916090-3105956dac38",
+    img: "/Img/pic5.webp",
     price: "$25",
   },
   {
     id: 4,
     name: "Casual Shirt",
-    img: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf",
+    img: "/Img/pic4.webp",
     price: "$35",
   },
   {
     id: 5,
     name: "Blue Jeans",
-    img: "https://images.unsplash.com/photo-1542272604-787c3835535d",
+    img: "/Img/pic3.webp",
     price: "$40",
   },
   {
     id: 6,
     name: "Black Hoodie",
-    img: "https://images.unsplash.com/photo-1618354691373-d851c5c3a990",
+    img: "/Img/pic2.webp",
     price: "$50",
   },
 ];
